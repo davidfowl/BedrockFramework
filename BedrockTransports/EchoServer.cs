65029677222768a04be818388feefd21eea97812
David Fowler <davidfowl@gmail.com>
2019-09-20 00:50:18 -0700

    Added more server logging

--- BedrockTransports/EchoServer.cs
+++ BedrockTransports/EchoServer.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
+using Microsoft.Extensions.Logging;
 
 namespace BedrockTransports
 {
     public class EchoServer : ConnectionHandler
     {
-        public override Task OnConnectedAsync(ConnectionContext connection)
+        private readonly ILogger<EchoServer> _logger;
+
+        public EchoServer(ILogger<EchoServer> logger)
         {
-            return connection.Transport.Input.CopyToAsync(connection.Transport.Output);
+            _logger = logger;
+        }
+
+        public override async Task OnConnectedAsync(ConnectionContext connection)
+        {
+            try
+            {
+                _logger.LogInformation("{ConnectionId} connected", connection.ConnectionId);
+
+                await connection.Transport.Input.CopyToAsync(connection.Transport.Output);
+            }
+            finally
+            {
+                _logger.LogInformation("{ConnectionId} disconnected", connection.ConnectionId);
+            }
         }
     }
 }
