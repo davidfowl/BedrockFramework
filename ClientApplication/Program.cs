48b006b74a87518ae2e56a7ef4c4c5d5f3ab82da
David Fowler <davidfowl@gmail.com>
2019-09-24 00:49:07 -0700

    Added Mqtt sample

--- ClientApplication/Program.cs
+++ ClientApplication/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Pipelines;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Bedrock.Framework;
@@ -7,6 +8,10 @@ using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Subscribing;
 
 namespace ClientApplication
 {
@@ -25,7 +30,7 @@ namespace ClientApplication
         }
     }
 
-    public class ClientApplication : BackgroundService
+    public class ClientApplication : BackgroundService, IMqttApplicationMessageReceivedHandler
     {
         private readonly ILoggerFactory _loggerFactory;
 
@@ -34,18 +39,37 @@ namespace ClientApplication
             _loggerFactory = loggerFactory;
         }
 
+        public Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            return Task.CompletedTask;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var clientFactory = new WebSocketConnectionFactory(_loggerFactory);
-            var clientEndPoint = new UriEndPoint(new Uri("https://localhost:5003"));
+            var options = new MqttClientOptionsBuilder()
+                        .WithClientId("Client1")
+                        .WithTcpServer("127.0.0.1", 5008)
+                        .Build();
 
-            var connection = await clientFactory.ConnectAsync(clientEndPoint);
-            Console.WriteLine($"Connected to {clientEndPoint}");
+            var factory = new MqttFactory();
+            var client = factory.CreateMqttClient();
+            await client.ConnectAsync(options, stoppingToken);
+            var subs = new MqttClientSubscribeOptions();
+            subs.TopicFilters.Add(new TopicFilter { Topic = "A" });
+            await client.SubscribeAsync(subs, stoppingToken);
+            await client.PublishAsync(new MqttApplicationMessage() { Topic = "A", Payload = Encoding.UTF8.GetBytes("Hello World") }, stoppingToken);
+            client.ApplicationMessageReceivedHandler = this;
 
-            Console.WriteLine("Echo server running, type into the console");
+            //var clientFactory = new WebSocketConnectionFactory(_loggerFactory);
+            //var clientEndPoint = new UriEndPoint(new Uri("https://localhost:5003"));
 
-            _ = Console.OpenStandardInput().CopyToAsync(connection.Transport.Output, stoppingToken);
-            await connection.Transport.Input.CopyToAsync(Console.OpenStandardOutput(), stoppingToken);
+            //var connection = await clientFactory.ConnectAsync(clientEndPoint);
+            //Console.WriteLine($"Connected to {clientEndPoint}");
+
+            //Console.WriteLine("Echo server running, type into the console");
+
+            //_ = Console.OpenStandardInput().CopyToAsync(connection.Transport.Output, stoppingToken);
+            //await connection.Transport.Input.CopyToAsync(Console.OpenStandardOutput(), stoppingToken);
         }
 
     }
--- ServerApplication/Program.cs
+++ ServerApplication/Program.cs
@@ -1,12 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Bedrock.Framework;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MQTTnet.Adapter;
+using MQTTnet.AspNetCore;
+using MQTTnet.Packets;
+using MQTTnet.Protocol;
 
 namespace ServerApplication
 {
@@ -41,12 +47,14 @@ namespace ServerApplication
                                //var connectionString = context.Configuration["AzureSignalR:ConnectionString"];
                                //options.ListenAzureSignalR(connectionString, "myhub",
                                //    builder => builder.UseConnectionHandler<EchoServerApplication>());
-
                                // SignalR on TCP
                                options.Listen(IPAddress.Loopback, 5006, builder => builder.UseHub<Chat>());
 
                                // HTTP/1.1 server
                                options.Listen(IPAddress.Loopback, 5007, builder => builder.UseHttpServer(new HttpApplication()));
+
+                               // MQTT application
+                               options.Listen(IPAddress.Loopback, 5008, builder => builder.UseConnectionHandler<MqttApplication>());
                            })
                            .Build();
 
@@ -59,6 +67,55 @@ namespace ServerApplication
             {
                 var responseData = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello World");
                 await context.Output.WriteAsync(responseData);
+            }
+        }
+
+        public class MqttApplication : MqttConnectionHandler
+        {
+            public MqttApplication()
+            {
+                ClientHandler = OnClientConnectedAsync;
+            }
+
+            private async Task OnClientConnectedAsync(IMqttChannelAdapter adapter)
+            {
+                while (true)
+                {
+                    var packet = await adapter.ReceivePacketAsync(Timeout.InfiniteTimeSpan, default);
+
+                    switch (packet)
+                    {
+                        case MqttConnectPacket connectPacket:
+                            await adapter.SendPacketAsync(new MqttConnAckPacket
+                            {
+                                ReturnCode = MqttConnectReturnCode.ConnectionAccepted,
+                                ReasonCode = MqttConnectReasonCode.Success,
+                                IsSessionPresent = false
+                            }, Timeout.InfiniteTimeSpan,
+                            default);
+                            break;
+                        case MqttDisconnectPacket disconnectPacket:
+                            break;
+                        case MqttAuthPacket mqttAuthPacket:
+                            break;
+                        case MqttConnAckPacket connAckPacket:
+                            break;
+                        case MqttPublishPacket mqttPublishPacket:
+                            break;
+                        case MqttSubscribePacket mqttSubscribePacket:
+                            var ack = new MqttSubAckPacket
+                            {
+                                PacketIdentifier = mqttSubscribePacket.PacketIdentifier,
+                                ReturnCodes = new List<MqttSubscribeReturnCode> { MqttSubscribeReturnCode.SuccessMaximumQoS0 }
+                            };
+                            ack.ReasonCodes.Add(MqttSubscribeReasonCode.GrantedQoS0);
+
+                            await adapter.SendPacketAsync(ack, Timeout.InfiniteTimeSpan, default);
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
     }
