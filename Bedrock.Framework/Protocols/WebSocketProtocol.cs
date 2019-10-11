817659c49bc07dbb5c5179b5645b1516fc501740
David Fowler <davidfowl@gmail.com>
2019-10-10 22:55:43 -0700

    Add websocket protocol (it doesn't do much yet)

---
+++ Bedrock.Framework/Protocols/WebSocketProtocol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.WebSockets;
+using Bedrock.Framework.Infrastructure;
+using Microsoft.AspNetCore.Connections;
+
+namespace Bedrock.Framework.Protocols
+{
+    public class WebSocketProtocol
+    {
+        public WebSocketProtocol(WebSocket websocket, ConnectionContext connection)
+        {
+            WebSocket = websocket;
+            Connection = connection;
+        }
+
+        public WebSocket WebSocket { get; }
+        public ConnectionContext Connection { get; }
+
+        public static WebSocketProtocol CreateFromConnection(ConnectionContext connection, bool isServer, string subProtocol, TimeSpan keepAliveInterval)
+        {
+            var websocket = WebSocket.CreateFromStream(new DuplexPipeStream(connection.Transport.Input, connection.Transport.Output), isServer, subProtocol, keepAliveInterval);
+            return new WebSocketProtocol(websocket, connection);
+        }
+    }
+}
