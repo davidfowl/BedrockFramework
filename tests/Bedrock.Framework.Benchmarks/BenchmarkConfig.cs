fd723d6d697386231ef8ead6b76dbb2b245583f3
alistairjevans <alistairjevans@gmail.com>
2020-01-04 14:18:35 +0000

    Initial pass at a basic benchmark for the protocol reader's message processing.

---
+++ tests/Bedrock.Framework.Benchmarks/BenchmarkConfig.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using System;
+using System.IO;
+
+namespace Bedrock.Framework.Benchmarks
+{
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(DefaultConfig.Instance);
+            Add(MemoryDiagnoser.Default);
+
+            ArtifactsPath = Path.Combine(AppContext.BaseDirectory, "artifacts", DateTime.Now.ToString("yyyy-mm-dd_hh-MM-ss"));
+        }
+    }
+}
---
+++ tests/Bedrock.Framework.Benchmarks/ProtocolReaderBenchmarks.cs
@@ -0,0 +1,93 @@
+﻿using Bedrock.Framework.Protocols;
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Buffers;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+
+namespace Bedrock.Framework.Benchmarks
+{
+    public class ProtocolReaderBenchmarks
+    {
+        private Pipe dataPipe;
+        private SimpleDelimitedReader delimitedMessageReader;
+        
+        [Params(1, 10, 100, 1000, 10000)]
+        public int ChunkCount { get; set; }
+
+        [Params(10)]
+        public int ChunkSize { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            dataPipe = new Pipe();
+
+            delimitedMessageReader = new SimpleDelimitedReader(ChunkSize);
+        }
+
+        class SimpleDelimitedReader : IMessageReader<int>
+        {
+            private readonly int chunkSize;
+            
+
+            public SimpleDelimitedReader(int chunkSize)
+            {
+                this.chunkSize = chunkSize;
+            }
+
+            public bool TryParseMessage(in ReadOnlySequence<byte> input, ref SequencePosition consumed, ref SequencePosition examined, out int message)
+            {
+                if(input.IsEmpty)
+                {
+                    message = 0;
+                    return false;
+                }
+
+                // Move along in the set.
+                consumed = input.GetPosition(chunkSize);
+                examined = consumed;
+
+                message = chunkSize;
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Benchmark reading a stream consisting of chunks of pre-determined lengths of data.
+        /// </summary>
+        [Benchmark]
+        public async ValueTask ReadChunkedProtocol()
+        {
+            var writeMemory = dataPipe.Writer.GetMemory(ChunkSize * ChunkCount);
+            // Tell the pipe we've put all the required data in memory (we haven't).
+            dataPipe.Writer.Advance(ChunkSize * ChunkCount);
+            dataPipe.Writer.Complete();
+            
+            var reader = new ProtocolReader(dataPipe.Reader);
+#if DEBUG
+            int chunkCounts = 0;
+#endif
+            // Read the chunks.
+            while(!(await reader.ReadAsync(delimitedMessageReader)).IsCompleted)
+            {
+#if DEBUG
+                chunkCounts++;
+#endif
+                reader.Advance();
+            }
+
+#if DEBUG
+            if(chunkCounts != ChunkCount)
+            {
+                throw new ApplicationException("Didn't read all the chunks");
+            }
+#endif
+            dataPipe.Reader.Complete();
+
+            // Reset the pipe back to 0.
+            dataPipe.Reset();
+        }
+    }
+}
