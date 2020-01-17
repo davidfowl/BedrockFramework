5d1520c9f5b9af1c60886e37d72368ab0be1387e
Tim Seaward <seawardtim@gmail.com>
2020-01-17 00:00:29 +0000

    Fixed to using sizeof to reduce Jitter Size

--- src/Bedrock.Framework.Experimental/Infrastructure/Endianness.cs
+++ src/Bedrock.Framework.Experimental/Infrastructure/Endianness.cs
@@ -20,34 +20,49 @@ namespace Bedrock.Framework.Experimental.Infrastructure
             return value;
         }
 
-        public static T Reverse<T>(T value) where T : unmanaged
+        public static unsafe T Reverse<T>(T value) where T : unmanaged
         {
-            var len = Unsafe.SizeOf<T>();
-            if (len == 1)
+            if (sizeof(T) == 1)
             {
                 return value;
             }
-            else if (len == 2)
+            else if (sizeof(T) == 2)
             {
                 var val = BinaryPrimitives.ReverseEndianness(Unsafe.As<T, ushort>(ref value));
                 return Unsafe.As<ushort, T>(ref val);
             }
-            else if (len == 4)
+            else if (sizeof(T) == 4)
             {
                 var val = BinaryPrimitives.ReverseEndianness(Unsafe.As<T, uint>(ref value));
                 return Unsafe.As<uint, T>(ref val);
             }
-            else if (len == 8)
+            else if (sizeof(T) == 8)
             {
                 var val = BinaryPrimitives.ReverseEndianness(Unsafe.As<T, ulong>(ref value));
                 return Unsafe.As<ulong, T>(ref val);
             }
-            else if (len < 512)
+            else if (sizeof(T) < 512)
             {
-                unsafe
+                var len = sizeof(T);
+                var val = stackalloc byte[len];
+                Unsafe.Write(val, value);
+                int to = len >> 1, dest = len - 1;
+                for (var i = 0; i < to; i++)
                 {
-                    var val = stackalloc byte[len];
-                    Unsafe.Write(val, value);
+                    var tmp = val[i];
+                    val[i] = val[dest];
+                    val[dest--] = tmp;
+                }
+                return Unsafe.Read<T>(val);
+            }
+            else
+            {
+                var len = sizeof(T);
+                var val = new byte[len];
+
+                fixed (void* valPointer = val)
+                {
+                    Unsafe.Write(valPointer, value);
                     int to = len >> 1, dest = len - 1;
                     for (var i = 0; i < to; i++)
                     {
@@ -55,30 +70,10 @@ namespace Bedrock.Framework.Experimental.Infrastructure
                         val[i] = val[dest];
                         val[dest--] = tmp;
                     }
-                    return Unsafe.Read<T>(val);
-                }
-            }
-            else
-            {
-                var val = new byte[len];
-                unsafe
-                {
-                    fixed (void* valPointer = val)
-                    {
-                        Unsafe.Write(valPointer, value);
-                        int to = len >> 1, dest = len - 1;
-                        for (var i = 0; i < to; i++)
-                        {
-                            var tmp = val[i];
-                            val[i] = val[dest];
-                            val[dest--] = tmp;
-                        }
 
-                        return Unsafe.Read<T>(valPointer);
-                    }
+                    return Unsafe.Read<T>(valPointer);
                 }
             }
         }
-
     }
 }
