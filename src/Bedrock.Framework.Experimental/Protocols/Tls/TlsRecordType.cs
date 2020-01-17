557164e9326084c13dcbb19040fd8fe5b51ef791
Tim Seaward <seawardtim@gmail.com>
2020-01-17 20:44:58 +0000

    Fix for rename

---
+++ src/Bedrock.Framework.Experimental/Protocols/Tls/TlsRecordType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bedrock.Framework.Experimental.Protocols.Tls
+{
+    // From RFC 8446 Appendix B.1 https://tools.ietf.org/html/rfc8446#appendix-B.1
+    internal enum TlsRecordType : byte
+    {
+        ChangeCipherSpec = 20,
+        Alert = 21,
+        Handshake = 22,
+        AppData = 23,
+        Invalid = 255,
+        Incomplete = 0,
+    }
+}
