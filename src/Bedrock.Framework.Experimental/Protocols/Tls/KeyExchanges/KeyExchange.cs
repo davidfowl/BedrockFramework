cbfa9ab0d1566444574e3f4dcd9e37c5d7baac74
Tim Seaward <seawardtim@gmail.com>
2020-01-18 10:54:23 +0000

    Removed custom certificate

--- src/Bedrock.Framework.Experimental/Protocols/Tls/KeyExchanges/KeyExchange.cs
+++ src/Bedrock.Framework.Experimental/Protocols/Tls/KeyExchanges/KeyExchange.cs
@@ -3,6 +3,7 @@ using Bedrock.Framework.Experimental.Protocols.Tls.Hashs;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Bedrock.Framework.Experimental.Protocols.Tls.KeyExchanges
@@ -10,7 +11,7 @@ namespace Bedrock.Framework.Experimental.Protocols.Tls.KeyExchanges
     internal abstract class KeyExchange : IDisposable
     {
         public abstract bool RequiresServerKeyExchange { get; }
-        public abstract void SetPeerKey(ReadOnlySequence<byte> peerKey, Certificate certificate, SignatureScheme scheme);
+        public abstract void SetPeerKey(ReadOnlySequence<byte> peerKey, X509Certificate2 certificate, SignatureScheme scheme);
         public abstract void SetPeerKey(ReadOnlySequence<byte> sharedKey);
         public abstract int KeyExchangeSize { get; }
         public abstract int WritePublicKey(Span<byte> keyBuffer);
@@ -19,7 +20,7 @@ namespace Bedrock.Framework.Experimental.Protocols.Tls.KeyExchanges
         public abstract void DeriveMasterSecret(HashProvider hashProvider, HashType hashType, ReadOnlySpan<byte> seed, Span<byte> output);
         public abstract void Dispose();
 
-        public void SetPeerKey(SequenceReader<byte> peerKey, Certificate certificate, SignatureScheme scheme)
+        public void SetPeerKey(SequenceReader<byte> peerKey, X509Certificate2 certificate, SignatureScheme scheme)
         {
             var buffer = peerKey.Sequence.Slice(peerKey.Position);
             SetPeerKey(buffer, certificate, scheme);
