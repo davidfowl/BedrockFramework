b38dc51a552c98454f43561f2d8f3a3476e361f3
yair halberstadt <yairhalberstadt@gmail.com>
2020-02-02 07:59:14 +0200

    Add HttpHeaderReaderBenchmarks

---
+++ tests/Bedrock.Framework.Benchmarks/HttpHeaderReaderBenchmarks.cs
@@ -0,0 +1,38 @@
+﻿using Bedrock.Framework.Protocols.Http.Http1;
+using BenchmarkDotNet.Attributes;
+using System;
+using System.Buffers;
+using System.Text;
+
+namespace Bedrock.Framework.Benchmarks
+{
+    public class HttpHeaderReaderBenchmarks
+    {
+        [Params(10, 1000)]
+        public int HeaderValueLength { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var rand = new Random(42);
+            var stringbuilder = new StringBuilder(HeaderValueLength);
+            for(var i = 0; i < HeaderValueLength; i++)
+            {
+                var offset = rand.Next() % 26;
+                stringbuilder.Append((char)('a' + offset));
+            }
+            bytes = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes($"Header-Name:{stringbuilder}\r\n"));
+        }
+
+        private ReadOnlySequence<byte> bytes;
+        private Http1HeaderReader reader = new Http1HeaderReader();
+
+        [Benchmark]
+        public bool ReadHeader()
+        {
+            var consumed = default(SequencePosition);
+            var examined = default(SequencePosition);
+            return reader.TryParseMessage(in bytes, ref consumed, ref examined, out var result);
+        }
+    }
+}
