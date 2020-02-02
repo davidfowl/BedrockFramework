ac04726b8aa97f51dd11568ee6ec19cf8dbf3bc2
yair halberstadt <yairhalberstadt@gmail.com>
2020-02-02 08:33:10 +0200

    Don't use random data in tests

--- tests/Bedrock.Framework.Benchmarks/HttpHeaderReaderBenchmarks.cs
+++ tests/Bedrock.Framework.Benchmarks/HttpHeaderReaderBenchmarks.cs
@@ -14,14 +14,8 @@ namespace Bedrock.Framework.Benchmarks
         [GlobalSetup]
         public void Setup()
         {
-            var rand = new Random(42);
-            var stringbuilder = new StringBuilder(HeaderValueLength);
-            for(var i = 0; i < HeaderValueLength; i++)
-            {
-                var offset = rand.Next() % 26;
-                stringbuilder.Append((char)('a' + offset));
-            }
-            bytes = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes($"Header-Name:{stringbuilder}\r\n"));
+            var data = "/search?rlz=1C1GCEA_enIL874IL874&sxsrf=ACYBGNQAjkw0VCQs1ElFY43_D_Wq9DMwQA%3A1580624707397&ei=Q2s2XtjxF8_RwAK35ocQ&q=request+header+example&oq=request+header+example&gs_l=psy-ab.3..0i7i30l10.9636.9636..9981...0.2..0.245.245.2-1......0....1..gws-wiz.......0i71.JfZoTvof620&ved=0ahUKEwiYn9TxnbLnAhXPKFAKHTfzAQIQ4dUDCAs&uact=5/search?rlz=1C1GCEA_enIL874IL874&sxsrf=ACYBGNQAjkw0VCQs1ElFY43_D_Wq9DMwQA%3A1580624707397&ei=Q2s2XtjxF8_RwAK35ocQ&q=request+header+example&oq=request+header+example&gs_l=psy-ab.3..0i7i30l10.9636.9636..9981...0.2..0.245.245.2-1......0....1..gws-wiz.......0i71.JfZoTvof620&ved=0ahUKEwiYn9TxnbLnAhXPKFAKHTfzAQIQ4dUDCAs&uact=5/search?rlz=1C1GCEA_enIL874IL874&sxsrf=ACYBGNQAjkw0VCQs1ElFY43_D_Wq9DMwQA%3A1580624707397&ei=Q2s2XtjxF8_RwAK35ocQ&q=request+header+example&oq=request+header+example&gs_l=psy-ab.3..0i7i30l10.9636.9636..9981...0.2..0.245.245.2-1......0....1..gws-wiz.......0i71.JfZoTvof620&ved=0ahUKEwiYn9TxnbLnAhXPKFAKHTfzAQIQ4dUDCAs&uact=5/search?rlz=1C1GCEA_enIL874IL874&sxsrf=ACYBGNQAjkw0VCQs1ElFY43_D_Wq9DMwQA%3A1580624707397&ei=Q2s2XtjxF8_RwAK35ocQ&q=request+header+example&oq=request+header+example&gs_l=psy-ab.3..0i7i30l10.9636.9636..9981...0.2..0.245.245.2-1......0....1..gws-wiz.......0i71.JfZoTvof620&ved=0ahUKEwiYn9TxnbLnAhXPKFAKHTfzAQIQ4dUDCAs&uact=5";
+            bytes = new ReadOnlySequence<byte>(Encoding.ASCII.GetBytes($"Header-Name:{data[..HeaderValueLength]}\r\n"));
         }
 
         private ReadOnlySequence<byte> bytes;
@@ -30,8 +24,8 @@ namespace Bedrock.Framework.Benchmarks
         [Benchmark]
         public bool ReadHeader()
         {
-            var consumed = default(SequencePosition);
-            var examined = default(SequencePosition);
+            var consumed = bytes.Start;
+            var examined = bytes.Start;
             return reader.TryParseMessage(in bytes, ref consumed, ref examined, out var result);
         }
     }
