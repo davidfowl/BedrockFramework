459da272d9b223c28a07ea467ec1e13fb37bb241
Adam Radocz <adam.radocz@gmail.com>
2022-01-15 21:09:16 +0100

    Add UDP client.

---
+++ UdpServerApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpServerApplication
+{
+    internal class Program
+    {
+        private const int listenPort = 5007;
+
+        static void Main(string[] args)
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, listenPort);
+
+            Console.WriteLine($"UDP Server. Waiting for broadcast on {listenPort}");
+
+            try
+            {
+                while (true)
+                {
+                    byte[] bytes = listener.Receive(ref endPoint);
+
+                    Console.WriteLine($"Received broadcast from {endPoint} :");
+                    Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
--- src/Bedrock.Framework/Transports/Sockets/SocketConnectionFactory.cs
+++ src/Bedrock.Framework/Transports/Sockets/SocketConnectionFactory.cs
@@ -11,9 +11,18 @@ namespace Bedrock.Framework
 {
     public class SocketConnectionFactory : IConnectionFactory
     {
+        private readonly SocketType _socketType;
+        private readonly ProtocolType _protocolType;
+
+        public SocketConnectionFactory(SocketType socketType, ProtocolType protocolType)
+        {
+            _socketType = socketType;
+            _protocolType = protocolType;
+        }
+
         public ValueTask<ConnectionContext> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken = default)
         {
-            return new SocketConnection(endpoint).StartAsync();
+            return new SocketConnection(endpoint, _socketType, _protocolType).StartAsync();
         }
     }
 }
